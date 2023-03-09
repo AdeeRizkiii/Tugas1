cfab1d481e6c46ecfc0cc832d4a1c030df127b55
AdeeRizkiii <126134837+AdeeRizkiii@users.noreply.github.com>
2023-03-09 12:15:27 +0700

    Add files via upload

---
+++ bool.php
@@ -0,0 +1,10 @@
+<?php
+
+
+echo " Apakah agung ganteng ? ";
+var_dump (true);
+
+echo "Apakah ikan berjalan ?";
+var_dump (false);
+
+?>
---
+++ variabel.php
@@ -0,0 +1,20 @@
+<?php
+
+$name = "Ade Rizki";
+$age = 21;
+$alamat = "jl.Rancabali ";
+
+
+echo "Nama : ";
+echo $name;
+echo "\n";
+
+echo "Usia :";
+echo $age;
+echo "\n";
+
+echo "alamat :";
+echo $alamat;
+
+
+?>
